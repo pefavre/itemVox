5415065beeeaf28ad935efa85a39330b55c06a8e
Pierre Etienne Favre <pierre.etienne.favre@gmail.com>
2011-11-05 15:34:06 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItemVox::Application.config.secret_token = '8ba3b11237c551223ecf206150aa3d3ec81392c934ee64b430df74ac485e4506c9edb206d6be3b72f2857349af1dc02c4acf5c63fa186bf5895a2b7954f18257'
