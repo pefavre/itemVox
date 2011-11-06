6327f87c07c9d98c2849f7026e6823f12fc730ed
Pierre-Rudolf Gerlach <pierre@joshfire.com>
2011-11-06 06:50:26 +0100

    associate some tags with some albums

--- object/mirror.py
+++ object/mirror.py
@@ -2,8 +2,23 @@
 
 import sys, re, os
 
+associations = {
+	'0008d00218bd6e5669d7' : 'spotify:album:3Y8O9H5qRkyqYEgwNhzwUW' # autumn falling
+	, '0008d00218c10916b3a3' : 'spotify:album:5iQkt7Mx6B0oWTMhZoKHYC' # back to the future
+	, '0008d00218c10916c6df' : 'spotify:album:0nMpXqkS4Attun6WWrHGvv' # hot club du france 1947
+	, '0008d00218bd6e566cc8' : 'spotify:album:73atzd4xK9PMS4iGanT9un' # joplin
+	, '0008d00218bd6e566d08' : 'spotify:album:7exqkn1MEoUhfDRMjwCOgm' # fucked up
+	, '0008d00218bd6e566ead' : 'spotify:album:0PTD76vhQLnOSdmTfH3qg1' # asian dub
+	, '0008d0021a0353184589' : 'spotify:album:4McrRTxFA51hl3Gy8Er2da' # lapin bleu
+	, '0008d00218bd6e566eed' : 'spotify:album:1zRIx14J26U6SANgfShypH' # lanterns on the lake
+	, '0008d00218c10916bdc0' : 'spotify:album:0CgTKS2HmWc3JeI9Fit2vX' # barbershop / sweeney todd
+	, '0008d00218bd6e566997' : 'spotify:album:32ZaJ7p8xvsYE8udh6vyOg' # joe dassin
+}
+
+
 def findUriForTag(tag):
-	return 'spotify:album:6q3hbicXXa7IyXygYnc1rg'
+	uri = associations[tag]
+	return uri
 
 def openUri(uri):
 	cmd = ('./scripts/openAlbum.sh %s ; open ./scripts/play.app' % uri)
